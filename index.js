d31379510859acdb4a7edba7fbdad64627efc7d9
corpydev <szymanskiszy@gmail.com>
2023-11-25 12:39:31 +0100

    initial commit for local-dev-server

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+import { addAppRouters } from "./src/app/index.js";
+import morgan from "morgan";
 
 const app = express();
 
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ message: "Welcome to api!" });
 });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.all("/api", (req, res) => {
+  res.send({ message: "Welcome to api!" });
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const routes = await addAppRouters();
+app.use("/api", routes);
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on("error", console.error);
 
 module.exports = app;
---
+++ src/app/todos.js
@@ -0,0 +1,167 @@
+import { Router } from "express";
+import fs from "fs";
+import path from "path";
+
+const dbDir = path.join(process.cwd(), "src", "db");
+const todosFilePath = path.join(dbDir, "todos.json");
+
+async function createTodos() {
+  const todos = [
+    {
+      id: 1,
+      title: "Todo 1",
+      createdAt: "2021-05-22T11:20:22.935Z",
+      author: "Anonymous",
+      isDone: false,
+      note: "Done the course",
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      createdAt: "2022-05-22T11:20:22.935Z",
+      doneDate: "2022-05-22T11:20:22.935Z",
+      author: "Anonymous",
+      isDone: true,
+      note: "Register to course",
+    },
+  ];
+  try {
+    fs.readFileSync(todosFilePath, "utf8");
+  } catch (error) {
+    await saveTodos(todos, 3);
+  }
+}
+
+async function getTodos() {
+  const file = fs.readFileSync(todosFilePath, "utf8");
+  return JSON.parse(file);
+}
+
+async function saveTodos(todos, id) {
+  const newTodos = {
+    todos,
+    id,
+  };
+  try {
+    fs.mkdirSync(dbDir);
+  } catch (e) {
+    //nothing to do folder already exists
+  }
+
+  fs.writeFileSync(todosFilePath, JSON.stringify(newTodos));
+}
+
+export async function addTodoRoutes() {
+  await createTodos();
+  const router = Router();
+
+  router.get("/", async (req, resp) => {
+    const { todos } = await getTodos();
+    resp.send(todos);
+  });
+
+  router.post("/", async (req, resp) => {
+    const { todos, id } = await getTodos();
+
+    const note = req.body.note || "";
+    const author = req.body.author || "Anonymous";
+    const createdAt = new Date().toISOString();
+    const title = req.body.title || `Todo ${id}`;
+
+    const newTodo = {
+      id,
+      title,
+      note,
+      author,
+      isDone: false,
+      createdAt,
+    };
+
+    todos.push(newTodo);
+    await saveTodos(todos, id + 1);
+
+    resp.send(newTodo);
+  });
+
+  router.get("/:id", async (req, resp) => {
+    const { todos } = await getTodos();
+    const foundTodo = todos.find((todo) => todo.id === +req.params.id);
+
+    if (!foundTodo) {
+      return resp.status(404).send({ message: "Todo not found" });
+    }
+
+    resp.send(foundTodo);
+  });
+
+  router.put("/:id/markAsDone", async (req, resp) => {
+    const { todos, id } = await getTodos();
+    const index = todos.findIndex((todo) => todo.id === +req.params.id);
+
+    if (index < 0) {
+      return resp.status(404).send({ message: "Todo not found" });
+    }
+
+    const todo = todos[index];
+
+    if (todo?.isDone) {
+      return resp.status(400).send({ message: "Todo already done" });
+    }
+
+    const editedTodo = {
+      ...todo,
+      isDone: true,
+      doneDate: new Date().toISOString(),
+    };
+
+    todos[index] = editedTodo;
+
+    await saveTodos(todos, id);
+
+    resp.send(editedTodo);
+  });
+
+  router.put("/:id", async (req, resp) => {
+    const { todos, id } = await getTodos();
+    const index = todos.findIndex((todo) => todo.id === +req.params.id);
+
+    if (index < 0) {
+      return resp.status(404).send({ message: "Todo not found" });
+    }
+
+    const todo = todos[index];
+    const note = req.body.note || todo.note;
+    const author = req.body.author || todo.author;
+    const title = req.body.title ? `${req.body.title}` : todo.title;
+    const editedTodo = {
+      ...todo,
+      title,
+      note,
+      author,
+    };
+
+    todos[index] = editedTodo;
+
+    await saveTodos(todos, id);
+
+    resp.send(editedTodo);
+  });
+
+  router.delete("/:id", async (req, resp) => {
+    const { todos, id } = await getTodos();
+    const index = todos.findIndex((todo) => todo.id === +req.params.id);
+
+    if (index < 0) {
+      return resp.status(404).send({ message: "Todo not found" });
+    }
+
+    await saveTodos(
+      todos.filter((el) => el.id !== +req.params.id),
+      id
+    );
+
+    resp.send({ id: req.params.id });
+  });
+
+  return router;
+}

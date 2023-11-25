16ffebfcecdb718a84354f7054ae2b54143eb8df
corpydev <szymanskiszy@gmail.com>
2023-11-25 12:49:20 +0100

    returned to previous settings without server.listen

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-// import swaggerUi from "swagger-ui-express";
-// import swaggerDocument from "./swagger.json";
-// import { addAppRouters } from "./src/app/index.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+import { addAppRouters } from "./src/app/index.js";
 import morgan from "morgan";
 
 const app = express();
@@ -11,21 +11,17 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.all("/api", (req, res) => {
   res.send({ message: "Welcome to api!" });
 });
 
-// app.all("/api", (req, res) => {
-//   res.send({ message: "Welcome to api!" });
-// });
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// const routes = await addAppRouters();
-// app.use("/api", routes);
+const routes = await addAppRouters();
+app.use("/api", routes);
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
+
+app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
-server.on("error", console.error);
